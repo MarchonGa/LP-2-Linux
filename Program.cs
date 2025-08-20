7332475643297892cdc83460af2efeb91d0e7744
MarchonGa <marchonreboucas@gmail.com>
2025-08-20 20:08:56 -0300

    Implementação de PI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var x = 40;
-Console.WriteLine(x);
+﻿using System;
+namespace TourOfCsharp;
+
+class Program {
+    static void Main()
+    {
+        ulong tam = 1000000000;
+        Console.WriteLine(Pi(tam));
+    }
+
+    static double Pi(ulong n){
+        double x = 0f;
+
+        for (ulong i = 0; i <= n; i++)
+        {
+            x += (Math.Pow(-1, i) / (2 * i + 1));
+        }
+        return x*4;
+    }
+    
+
+}
